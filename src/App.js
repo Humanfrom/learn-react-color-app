eccd0c8b44d8436d37068ad262f5a1fff8e12be0
Humanfrom <leonovton@gmail.com>
2022-02-08 15:13:15 +0300

    Added class AddColorForm - form is sending to console

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class AddColorForm extends Component {
+
+  constructor(props){
+    super(props);
+    this.submit = this.submit.bind(this);
+  }
+
+  static propTypes = {
+    onNewColor: PropTypes.func
+  }
+
+  static defaultProps = {
+    onNewColor: f=>f
+  }
+
+  submit(e) {
+    const {_title, _color} = this.refs;
+    e.preventDefault();
+    this.props.onNewColor(_title.value,_color.value);
+    //alert(`New Color: ${_title.value} ${_color.value}`);
+    _title.value = '';
+    _color.value = '#000000'
+    _title.focus();
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.submit}>
+        <input type="text" ref="_title" placeholder="color title..." required/>
+        <input type="color" ref="_color" required/>
+        <button>ADD</button>
+      </form>
+    )
+  }
+}
+
+const logColor = (title,color) => console.log(`New Color: ${title} ${color}`)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AddColorForm onNewColor={logColor}/>
   );
 }
 
