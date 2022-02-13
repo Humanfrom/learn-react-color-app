d58de0e3d9d4078e913004c557624666bd57d398
Humanfrom <leonovton@gmail.com>
2022-02-13 20:02:02 +0300

    Add stars rating - two way StarRating (createClass and class ES6)

--- src/App.js
+++ src/App.js
@@ -1,49 +1,111 @@
 //import logo from './logo.svg';
 import './App.css';
-import {Component} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import createClass from "create-react-class";
 
-class AddColorForm extends Component {
+const logColor = (title,color) => console.log(`New Color: ${title} ${color}`)
+
+const Star = ({selected=false, onClick=f=>f}) =>
+<div className={(selected) ? "star selected" : "star"} onClick={onClick}></div>
+
+Star.propTypes = {
+  selected:PropTypes.bool,
+  onClick:PropTypes.func
+}
+
+class StarRating extends Component{
 
   constructor(props){
-    super(props);
-    this.submit = this.submit.bind(this);
+    super(props)
+    this.state = {
+      starsSelected: 0
+    }
+    this.change = this.change.bind(this);
   }
 
   static propTypes = {
-    onNewColor: PropTypes.func
+    totalStars: PropTypes.number
   }
 
   static defaultProps = {
-    onNewColor: f=>f
+    totalStars: 5
   }
 
-  submit(e) {
-    const {_title, _color} = this.refs;
+  change(starsSelected){
+    this.setState({starsSelected});
+  }
+
+  render(){
+    const {totalStars} = this.props;
+    const {starsSelected} = this.state;
+    return (
+      <div className="star-rating">
+      {[...Array(totalStars)].map((n,i) =>
+        <Star key={i}
+        selected={i<starsSelected}
+        onClick={() => this.change(i+1)}
+        />
+      )
+    }<br/>
+    <p>{starsSelected} звёзд из {totalStars}</p>
+    </div>
+  )
+  }
+}
+
+const StarRatingCC = createClass({
+  displayName:"StarRatingCC",
+  getDefaultProps(){
+    return{totalStars: 5}
+  },
+  getInitialState(){
+    return{starsSelected: 0}
+  },
+  componentDidMount(){
+    const {starsSelected} = this.props;
+    if(starsSelected){
+      this.setState({starsSelected})
+    }
+  },
+  change(starsSelected){
+    this.setState({starsSelected})
+  },
+  render(){
+    const {totalStars} = this.props;
+    const {starsSelected} = this.state;
+    return(
+      <div className="StarRating">
+      {[...Array(totalStars)].map((n,i) => <Star key={i} selected={i<starsSelected} onClick={() => this.change(i+1)}/>)}
+      <br/>
+      <p>{starsSelected} звёзд из {totalStars}</p>
+      </div>
+    )
+  }
+})
+
+
+const AddColorForm = ({onNewColor=f=>f}) => {
+  let _title, _color;
+  const submit = e => {
     e.preventDefault();
-    this.props.onNewColor(_title.value,_color.value);
-    //alert(`New Color: ${_title.value} ${_color.value}`);
+    onNewColor(_title.value,_color.value);
     _title.value = '';
     _color.value = '#000000'
     _title.focus();
   }
-
-  render(){
-    return(
-      <form onSubmit={this.submit}>
-        <input type="text" ref="_title" placeholder="color title..." required/>
-        <input type="color" ref="_color" required/>
-        <button>ADD</button>
-      </form>
-    )
-  }
+  return(
+    <form onSubmit={submit}>
+    <input type="text" ref={input => _title = input} placeholder="color title..." required/>
+    <input type="color" ref={input => _color = input} required/>
+    <button>ADD</button>
+    </form>
+  )
 }
-
-const logColor = (title,color) => console.log(`New Color: ${title} ${color}`)
 
 function App() {
   return (
-    <AddColorForm onNewColor={logColor}/>
+    <StarRatingCC totalStars={5} starsSelected={2}/>
   );
 }
 
